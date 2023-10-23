4591eb73e6584a4d1319036b917f533dd1aee937
Eduardo <98243709+EduardoDuduu@users.noreply.github.com>
2023-10-23 19:57:23 -0300

    Implementei a janela pop-up de cookies
    Adicionei uma janela de permissão de uso de cookies, e políticas de privacidade.
    Falta adicionar as páginas

---
+++ scripts/cookies.js
@@ -0,0 +1,38 @@
+function cookies(functions) {
+    const container = document.querySelector('.cookies-container');
+    const save = document.querySelector('.cookies-save');
+    if(!container || !save) return null;
+
+    const localPref = JSON.parse(window.localStorage.getItem('cookies-pref'));
+    if (localPref) activateFunctions(localPref);
+
+    function getFormPref() {
+        return [...document.querySelectorAll('[data-function]')].filter((el) => el.checked,).map((el) => el.getAttribute('data-function'));
+    }
+
+    function activateFunctions(pref) {
+        pref.forEach((f) => functions[f]());
+        container.style.display = 'none';
+        window.localStorage.setItem('cookies-pref', JSON.stringify(pref));
+    }
+
+    function handleSave() {
+        const pref = getFormPref();
+        activateFunctions(pref);
+    }
+
+    save.addEventListener('click', handleSave);
+}
+
+function analytics() {
+    console.log('Aceitou o Analytics');
+}
+
+function privacyPolicies() {
+    console.log('Aceitou a Política de Privacidade');
+}
+
+cookies({
+    analytics,
+    privacyPolicies,
+});
